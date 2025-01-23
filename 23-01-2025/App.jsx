b52c83a1ee007546ea637cfccae2e0cd3f8d62c2
Loga Prabakar VS <154336741+LogaPrabakar@users.noreply.github.com>
2025-01-23 12:27:55 +0530

    Add files via upload

---
+++ 23-01-2025/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FormValidate from "./FormValidate";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>React Form Validation</h1>
+      <FormValidate />
+    </div>
+  );
+};
+
+export default App;
